a8faa03219113971566ccf74d863abb44b8e26db
Clayton Bettini <clayton.bettini@gmail.com>
2021-05-06 22:04:32 -0300

    Graficos estaticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "comonents/BarChart";
 import DataTable from "comonents/DataTable";
+import DonutChart from "comonents/DonutChart";
 import Footer from "comonents/Footer";
 import NavBar from "comonents/NavBar";
 
@@ -7,8 +9,23 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Olá Mundo!</h1>
-        
+        <h1 className="text-primary py-3">Dashboard de Vendas!</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as Vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas vendas</h2>
+        </div>
+
         <DataTable />
       </div>
       <Footer />
